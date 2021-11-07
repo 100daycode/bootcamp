281955e11967022c1ab4ceb1477e6f423f83ca96
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-11-07 11:23:21 +0700

    week 1

---
+++ phase-1/calculator/app.js
@@ -0,0 +1,5 @@
+const calculator = require("./mathoperation");
+console.log(calculator.add(2, 2));
+console.log(calculator.substract(2, 2));
+console.log(calculator.divider(10, 2));
+console.log(calculator.multiply(6, 2));
